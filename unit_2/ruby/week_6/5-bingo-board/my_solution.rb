33305a99f25895582b43297fd9cd520dc67757f0
Eugene Lynch <eugenestapler@gmail.com>
2015-01-23 11:42:42 +1300

    Update my_solution.rb

--- unit_2/ruby/week_6/5-bingo-board/my_solution.rb
+++ unit_2/ruby/week_6/5-bingo-board/my_solution.rb
@@ -1,8 +1,6 @@
 # U2.W5: A Nested Array to Model a Bingo Board SOLO CHALLENGE
 
-
-# Release 0: Pseudocode
-# Outline:
+# Pseudocode
 
 # Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
   #fill in the outline here
@@ -19,7 +17,7 @@
 # Display the board to the console (prettily)
   #fill in the outline here
 
-# Release 1: Initial Solution
+# Your Solution
 
 class BingoBoard
 
@@ -30,17 +28,6 @@ class BingoBoard
     @bingo_board = Array.new(5) {Array(5.times.map{rand(1..100)})}
   end
 
-
 end
 
-# Release 3: Refactored Solution
-
-
-
-# Release 2: DRIVER TESTS GO BELOW THIS LINE
-
-
-
-# Release 4: Reflection
-
-
+# DRIVER TESTS GO BELOW THIS LINE
