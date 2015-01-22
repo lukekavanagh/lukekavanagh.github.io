502e96b5ddceccc1778354a5818878321c2beade
Eugene Lynch <eugenestapler@gmail.com>
2015-01-23 11:33:10 +1300

    Update my_solution.rb

--- unit_2/ruby/week_6/3-guessing-game/my_solution.rb
+++ unit_2/ruby/week_6/3-guessing-game/my_solution.rb
@@ -1,16 +1,4 @@
-# U2.W5: Build a simple guessing game
-
-
-# I worked on this challenge [by myself, with: ].
-
-# 2. Pseudocode
-
-# Input:
-# Output:
-# Steps:
-
-
-# 3. Initial Solution
+# Build a simple guessing game
 
 class GuessingGame
   def initialize(answer)
@@ -20,21 +8,4 @@ class GuessingGame
   # Make sure you define the other required methods, too
 end
 
-
-
-
-# 4. Refactored Solution
-
-
-
-
-
-
-# 1. DRIVER TESTS GO BELOW THIS LINE
-
-
-
-
-
-
-# 5. Reflection 
+# DRIVER TESTS GO BELOW THIS LINE
