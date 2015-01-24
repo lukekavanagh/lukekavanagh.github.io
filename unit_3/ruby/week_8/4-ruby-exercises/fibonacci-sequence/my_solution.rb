f844bdbcb30d34e738e72336edff1f81c28fa9d6
Eugene <eugenestapler@gmail.com>
2015-01-24 14:00:03 +1300

    added challenge folders and readmes

---
+++ unit_3/ruby/week_8/4-ruby-exercises/fibonacci-sequence/my_solution.rb
@@ -0,0 +1,32 @@
+# U3.W8-9:
+
+
+# I worked on this challenge [by myself, with: ].
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+def is_fibonacci?(num)
+
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+# DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# Reflection
---
+++ unit_3/ruby/week_8/4-ruby-exercises/fizz-buzz/my_solution.rb
@@ -0,0 +1,32 @@
+# U3.W8-9:
+
+# I worked on this challenge [by myself, with: ].
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+def super_fizzbuzz(array)
+
+
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+# DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# Reflection
