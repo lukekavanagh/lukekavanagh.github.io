b1f3bb28eb8478c352a4ca613b7d902c9de22ab2
Eugene Lynch <eugenestapler@gmail.com>
2015-01-24 14:47:19 +1300

    Update my_solution.rb

--- unit_3/ruby/week_8/6-nums-to-words/my_solution.rb
+++ unit_3/ruby/week_8/6-nums-to-words/my_solution.rb
@@ -1,29 +1,10 @@
-# U3.W8-9: Numbers to English Words
+# Numbers to English Words
 
 
-# I worked on this challenge [by myself, with: ].
-
-# Pseudocode
-
-
-
-# Initial Solution
-
-
-
-
-# Refactored Solution
-
+# Your Solution
 
 
 
 
 
 # DRIVER TESTS GO BELOW THIS LINE
-
-
-
-
-
-
-# Reflection
